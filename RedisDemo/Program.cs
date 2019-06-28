bc76c57571ca85111c85dacd43b966bd23f009d9
fengyhack <fengyhack@gmail.com>
2019-06-28 14:39:59 +0800

    redis demo

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var redisHelper = new RedisHelper("127.0.0.1:6379");
+            //redisHelper.SetStringValue("A", "123");
+            var result = redisHelper.GetStringValue("A");
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
